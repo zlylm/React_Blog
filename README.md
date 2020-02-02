9920912c349825e08a55a85dabf443349a4a3a42
zlylm <912924984@qq.com>
2020-02-02 17:28:57 +0800

    前端部分基本环节搭建

---
+++ README.md
@@ -0,0 +1,4 @@
+## 2020-2-2
+
+1. 安装基本环境
+2. @zeit/next-css 让next支持import的方式引入css文件
---
+++ blog/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+import {Button} from 'antd'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>My Blog</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Button>测试</Button>
+  </div>
+)
+
+export default Home
